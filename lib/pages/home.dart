87fb0f2ceb088faa15fffa2b0466b0408b2bfd8b
Aditya Patil <aditya4554patil@gmail.com>
2024-01-28 00:16:25 +0530

    1.Slider Added 2.Drawer Added 3.App Name and App Icon changed 4.Categories are added to SideBar(Drawer)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:my_news/models/article_model.dart';
-import 'package:my_news/models/category_news.dart';
 import 'package:my_news/models/catergory_model.dart';
 import 'package:my_news/pages/article_view.dart';
 import 'package:my_news/pages/categoryShowNews.dart';
 import 'package:my_news/services/data.dart';
-import 'package:my_news/services/show_news.dart';
 import '../services/news.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 class Home extends StatefulWidget{
   const Home({super.key});
 
@@ -20,6 +20,7 @@ class _HomeState extends State<Home>{
   List<ArticleModel> articles = [];
   // List<categoryNews> categoryArticles = [];
   bool _loading = true;
+  int activeIndex = 0;
 
 
   @override
@@ -54,16 +55,101 @@ class _HomeState extends State<Home>{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Flutter"),
-            Text("News",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),
-            ),
-          ],
+
+        title: Padding(
+          padding: const EdgeInsets.only(right: 28),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Flutter"),
+              Text("News",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
         centerTitle: true,
         elevation: 0.0,
+      ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Aditya Patil'), // Replace with actual user name
+                accountEmail: Text('adipatil07@gmail.com'),
+                // Replace with actual user email
+                currentAccountPicture: CircleAvatar(
+                  // Replace with an actual user photo
+                  backgroundImage: AssetImage('images/adi2.jpg'),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              //  DrawerHeader(
+              //   decoration: BoxDecoration(
+              //     color: Colors.blue,
+              //   ),
+              //   child: Text('FlutterNews',style: TextStyle(
+              //     fontSize: 20
+              //   ),),
+              // ),
+
+              ListTile(
+                title: const Text('General'),
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder:
+                  (context)=> categoryShowNews(name: "General"),
+                  ));
+                },
+              ),
+
+            ListTile(
+              title: const Text('Health'),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=> categoryShowNews(name: "Health"),
+                ));
+              },
+            ),
+
+            ListTile(
+              title: const Text('Business'),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=> categoryShowNews(name: "business"),
+                ));
+              },
+            ),
+
+            ListTile(
+              title: const Text('Entertainment'),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=> categoryShowNews(name: "Entertainment"),
+                ));
+              },
+            ),
+
+            ListTile(
+              title: const Text('Sports'),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=> categoryShowNews(name: "Sports"),
+                ));
+              },
+            ),
+
+            ListTile(
+              title: const Text('Science'),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=> categoryShowNews(name: "Science"),
+                ));
+              },
+            ),
+        ]
+        ),
       ),
       body: _loading ? Center(
           child: Container(
@@ -91,6 +177,62 @@ class _HomeState extends State<Home>{
           }),
           ),
 
+          //Breaking News Text
+          Container(
+            padding: EdgeInsets.all(12),
+
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //Icon(Icons.warning, color: Colors.black), // Example of using an icon
+                Text(
+                  "Breaking News !",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          //For SLider
+          CarouselSlider.builder(
+              itemCount: articles.length > 5 ? 5 : articles.length,
+                  itemBuilder: (context,index, realindex){
+                    final url = articles[index];
+                    return GestureDetector(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder:
+                            (context)=> ArticleView(
+                          blogurl: articles[index].url,
+                        )
+                        ));
+                      },
+                        child: buildImages(url,index)
+                    );
+
+                  },
+                  options: CarouselOptions(
+                      height: 250,
+                      autoPlay: true,
+                    enlargeCenterPage: true,
+                    initialPage: 1,
+                    autoPlayAnimationDuration: Duration(seconds: 1),
+                    enlargeStrategy: CenterPageEnlargeStrategy.height,
+                    onPageChanged: (index , reason ){
+                        setState(() {
+                          activeIndex = index;
+                        });
+                    },
+                  )
+              ),
+            SizedBox(height: 10,),
+            buildIndicator(),
+
+          SizedBox(height: 20,),
           ///For Blogs
           Container(
             padding: EdgeInsets.only(top: 16),
@@ -113,6 +255,47 @@ class _HomeState extends State<Home>{
       ),
     );
   }
+
+  Widget buildImages(ArticleModel article, int index) => Container(
+    margin: EdgeInsets.symmetric(horizontal: 12),
+
+    child: Stack(
+      children: [
+        ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              article.urltoimage,
+              fit: BoxFit.cover,
+              height: 250,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+        Container(
+          padding: EdgeInsets.only(left: 10,right: 10),
+          height: 250,
+          margin: EdgeInsets.only(top: 170),
+          decoration: BoxDecoration(color: Colors.black12,borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10))),
+          child:Text(
+            article.title, // Assuming 'title' is the property in your ArticleModel class
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+
+        // SizedBox(height: 8), // Adjust the spacing between image and heading
+
+      ],
+    ),
+  );
+
+      Widget buildIndicator()=> AnimatedSmoothIndicator(
+        activeIndex: activeIndex,
+        count : 5
+      );
+
 }
 
 class CategoryTile extends StatelessWidget{
@@ -147,21 +330,26 @@ class CategoryTile extends StatelessWidget{
           alignment: Alignment.center,
           width: 120,
           height: 60,
+          
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6),
               color: Colors.black38,
           ),
 
           child: Center(
-          child:Text(
-          categoryName,
-          style:const TextStyle(
-          color:Colors.white ,
-          fontSize: 14,
-          fontWeight:
-          FontWeight.bold
-          ) ,
-          ),
+
+            child:Text(
+
+              categoryName,
+              style:const TextStyle(
+              color:Colors.white ,
+              fontSize: 14,
+              fontWeight:
+              FontWeight.bold,
+                
+              ) ,
+
+            ),
         )
         ),
      ]
